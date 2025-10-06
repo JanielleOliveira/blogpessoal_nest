16e0a07a9d26aab6927e65309f43db752c5b79fc
JanielleOliveira <janielleoliver.brito@gmail.com>
2025-10-06 19:17:57 -0300

    Implementa documentação Swagger completa no projeto Blog Pessoal com configuração nos módulos Usuario, Tema, Auth e Postagem, integração na classe main, definição do Swagger como página principal e registro dos controllers no AppModule

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Blog Pessoal')
+    .setDescription(
+      'Projeto Blog Pessoal feito com NEST durante as aulas da Turma JS 09, Generation Brasil.',
+    )
+    .setContact(
+      'Janielle Oliveira Brito',
+      'https://github.com/JanielleOliveira/blogpessoal_nest',
+      'janielleoliver.brito@gmail.com',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
 
   process.env.TZ = '-03:00';
 
@@ -13,4 +30,5 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 4000);
 }
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
