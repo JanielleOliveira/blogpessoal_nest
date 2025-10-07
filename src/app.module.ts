611369da98c0e74c814be59a9ce0bc21bc6b8bf7
JanielleOliveira <janielleoliver.brito@gmail.com>
2025-10-07 01:41:46 -0300

    Atualização do Deploy - Blog Pessoal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
+import { ProdService } from './data/service/prod.service';
 import { PostagemModule } from './postagem/postagem.module';
 import { TemaModule } from './tema/tema.module';
 import { UsuarioModule } from './usuario/usuario.module';
-import { ConfigModule } from '@nestjs/config';
-import { ProdService } from './data/service/prod.service';
 
 @Module({
   imports: [
